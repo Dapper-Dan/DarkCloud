62752393fa4574dbd8049e16551b022667451ce9
dapper_dan <dlancaster08@gmail.com>
2020-04-30 15:58:02 -0700

    User auth finished

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+    validates :display_name, :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, :full_name, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    attr_reader :password
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+    private
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+end
+

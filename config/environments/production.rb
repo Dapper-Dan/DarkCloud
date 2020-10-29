5ca7b66a54176fc14fda4114d3cf56486dceb040
dapper_dan <dlancaster08@gmail.com>
2020-10-29 13:53:03 -0700

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.active_storage.service = :amazon_prod
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -39,6 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.active_storage.service = :amazon_prod
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.active_storage.service = :local#######################################################
 
